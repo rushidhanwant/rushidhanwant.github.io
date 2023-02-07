52311365de9ac422fe4da1d7a718bb0bcfcab6d5
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-02-07 14:40:30 +0530

    resolved project page scroll issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./Pages/LandingPage/LandingPage";
-import ProjectPage from "./Pages/ProjectDetailsPage/ProjectDetailPage";
+import ProjectDetailsPage from "./Pages/ProjectDetailsPage/ProjectDetailPage";
 import { GlobalStyle, AppWrapper } from "./Style";
 import { Projects } from "./api/projects";
+import { useEffect } from "react";
+import ProjectPage from "./Pages/ProjectPage/ProjectPage";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
@@ -12,10 +24,18 @@ function App() {
       <BrowserRouter>
         <GlobalStyle />
         <Header />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/devnode" element={<ProjectPage {...Projects[0]}/>} />
-          <Route path="/clipto" element={<ProjectPage {...Projects[1]}/>} />
+          <Route
+            path="/devnode"
+            element={<ProjectDetailsPage {...Projects[0]} />}
+          />
+          <Route
+            path="/clipto"
+            element={<ProjectDetailsPage {...Projects[1]} />}
+          />
+          <Route path="/projects" element={<ProjectPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
