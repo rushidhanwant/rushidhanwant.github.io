75bf9289bd6083a55e630d0053b522350d1d8af5
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-02-07 14:43:12 +0530

    ui-fix

--- src/components/Cards/Style.tsx
+++ src/components/Cards/Style.tsx
@@ -6,25 +6,33 @@ export const CollabCardContainer = styled.div`
   align-items: center;
   gap: 45px;
   padding: 46px 62px;
-  
+
   max-width: 280px;
   height: 450px;
-  border: 7px solid #FBFBFB;
+  border: 7px solid #fbfbfb;
   border-radius: 28px;
 
-  font-family: 'Advent Pro',sans-serif;
+  font-family: Raven Sans NBP;
   font-style: normal;
   font-weight: 700;
   font-size: 48px;
   line-height: 57px;
   text-align: center;
-  color: #FFFFFF;
+  color: #ffffff;
+  @media only screen and (max-width: 768px) {
+    height: auto;
+    padding: 46px 42px;
+  }
 `;
 
-export const ProjectCardWrapper = styled.div.attrs((props: { color: string }) => props)`
-  width: 250px;
-  height: 180px;
+export const ProjectCardWrapper = styled.div.attrs(
+  (props: { color: string }) => props
+)`
+  width: 100%;
+  max-width: 284px;
+  height: auto;
   padding: 25px;
-  background: ${(props) => props.color };
+  box-sizing: border-box;
+  background: ${(props) => props.color};
   border-radius: 28px;
 `;
